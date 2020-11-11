2672242dfd8428c41027d269d2772e53f244560e
Zhudongshawn <2686798745@qq.com>
2020-11-11 09:49:00 +0800

    Update the messages

--- app/Controllers/Travel.php
+++ app/Controllers/Travel.php
@@ -12,10 +12,40 @@ class Travel extends BaseController
     
         // get a template parser
     $parser = \Config\Services::parser();
+    $table = new \CodeIgniter\View\Table();
+    $headings = $places->fields;
+    $displayHeadings = array_slice($headings, 1, 2);
+    $table->setHeading(array_map('ucfirst', $displayHeadings));
+    
+    foreach ($records as $record) {
+    
+    $nameLink = anchor("travel/showme/$record->id",$record->name);
+    $table->addRow($nameLink,$record->description);
+    }
+    
+    
+    $template = [
+        'table_open' => '<table cellpadding="5px">',
+        'cell_start' => '<td style="border: 1px solid #dddddd;">', 
+        'row_alt_start' => '<tr style="background-color:#dddddd">',
+        ];
+    $table->setTemplate($template);
+    $fields = [
+        'title' => 'Travel Destinations',
+        'heading' => 'Travel Destinations', 
+        'footer' => 'Copyright Xavier(Zhudong He)'
+        ];
+    return $parser->setData($fields)
+      ->render('templates\top') .
+    $table->generate() .
+    $parser->setData($fields)
+      ->render('templates\bottom');
         // tell it about the substitions
     return $parser->setData(['records' => $records])
         // and have it render the template with those
     ->render('placeslist');
+    
+    
     }
     public function showme($id)
     {
@@ -25,10 +55,35 @@ class Travel extends BaseController
       $record = $places->find($id);
       // get a template parser
       $parser = \Config\Services::parser();
-      // tell it about the substitions
-      return $parser->setData($record)
+      $table = new \CodeIgniter\View\Table();
+      $headings = $places->fields;
+      
+      $table->addRow($record['id']);
+      $table->addRow($record['name']);
+      $table->addRow($record['description']);
+      $table->addRow($record['link']);
+      $table->addRow( "<img src='/image/".$record['image'].'\'/>');
+      
+      $template = [
+          'table_open' => '<table cellpadding="5px">',
+          'cell_start' => '<td style="border: 1px solid #dddddd;">', 
+          'row_alt_start' => '<tr style="background-color:#dddddd">',
+          ];
+      
+      $fields = [
+        'title' => 'This place',
+        'heading' => 'This place', 
+        'footer' => 'Copyright Zhudong He'
+        ];
+      
+      $table->setTemplate($template);
+      return $parser->setData($fields)
+         ->render('templates\top') .
+      $table->generate() .
+      $parser->setData($fields)
+         ->render('templates\bottom');
       // and have it render the template with those
-      ->render('oneplace');
+      
     }
 }
 
